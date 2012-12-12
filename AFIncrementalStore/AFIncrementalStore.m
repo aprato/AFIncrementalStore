6f0b9119d1055132f478b4afaf7a7162449dddfe
Mattt Thompson <m@mattt.me>
2012-12-12 11:13:32 -0800

    Removing stray subscripted mutable dictionary access

--- AFIncrementalStore/AFIncrementalStore.m
+++ AFIncrementalStore/AFIncrementalStore.m
@@ -778,7 +778,7 @@ inline NSString * AFResourceIdentifierFromReferenceObject(id referenceObject) {
         }
         
         NSMutableDictionary *objectIDsByResourceIdentifier = [_registeredObjectIDsByEntityNameAndNestedResourceIdentifier objectForKey:objectID.entity.name] ?: [NSMutableDictionary dictionary];
-        objectIDsByResourceIdentifier[AFResourceIdentifierFromReferenceObject(referenceObject)] = objectID;
+        [objectIDsByResourceIdentifier setObject:objectID forKey:AFResourceIdentifierFromReferenceObject(referenceObject)];
         
         [_registeredObjectIDsByEntityNameAndNestedResourceIdentifier setObject:objectIDsByResourceIdentifier forKey:objectID.entity.name];
     }
